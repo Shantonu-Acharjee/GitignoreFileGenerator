8f25e6aa1ec8a40d12fb10ac51ef410c1c7d619b
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-04-18 00:27:40 +0600

    All Done

--- Main.py
+++ Main.py
@@ -1,5 +1,23 @@
-#1.jpg, 2.mp4, 3.txt, 4.jpg
 import os
-size = os.stat("1.jpg").st_size
-#size = size / 1024
-print(size,'MB')
+PhotoDir = (input('Enter Your Folder Path : ')+ '/')
+Photos = os.listdir(PhotoDir)
+a = len(Photos)
+
+
+
+
+
+
+for i in range(0, a):
+    #print(Photos[i])
+    size = os.stat(PhotoDir+Photos[i]).st_size
+    size = size / 1000000
+    print(size,'MB')
+    if size >= 145:
+        try:
+            old_name = PhotoDir+Photos[i]
+            new_name = PhotoDir+"GitIgnore."+Photos[i]
+            os.rename(old_name, new_name)
+        except:
+            print('Rename Not Done.....!')
+
